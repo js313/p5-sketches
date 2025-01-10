27375e63706775c24102a254935ec0a1ebb95c14
js313 <sharmajeenit2000@gmail.com>
2025-01-10 08:36:24 +0530

    feat: generate and move random convex polygons

--- collisions/body.js
+++ collisions/body.js
@@ -1,31 +1,77 @@
 /// <reference path="../node_modules/@types/p5/global.d.ts" />
 
-class Body {
-  // Only circles supported, for now
-  constructor(centre, radius, mass, speed = 5) {
-    // Add mass
-    this.centre = centre;
-    this.radius = radius;
+class Polygon {
+  constructor(centre, numVertices, distFromCentre, mass, color, speed = 5) {
     this.mass = mass;
     this.speed = speed;
+    this.rotation = 0;
+    this.color = color;
+
     const rAngle = random(TWO_PI);
     this.velocity = createVector(cos(rAngle), sin(rAngle)).mult(this.speed);
+    this.centre = centre;
+    this.distFromCentre = distFromCentre;
+    this.numVertices = numVertices;
+    this.createShape(numVertices);
+  }
+
+  createShape() {
+    let randomAngles = [];
+
+    for (let i = 0; i < this.numVertices; i++) {
+      randomAngles.push(random(TWO_PI));
+    }
+    randomAngles.sort();
+    this.vertices = [];
+    for (let i = 0; i < this.numVertices; i++) {
+      let vert = createVector(
+        this.centre.x + this.distFromCentre * cos(randomAngles[i]),
+        this.centre.y + this.distFromCentre * sin(randomAngles[i])
+      );
+
+      this.vertices.push(vert);
+    }
+
+    this.calculateEdges();
+  }
+
+  calculateEdges() {
+    this.edges = [];
+    for (let i = 1; i < this.numVertices; i++) {
+      let edge = this.vertices[i].copy().sub(this.vertices[i - 1]);
+      this.edges.push(edge);
+    }
+    let closingEdge = this.vertices[0]
+      .copy()
+      .sub(this.vertices[this.numVertices - 1]);
+    this.edges.push(closingEdge);
   }
 
   draw() {
-    stroke(200);
-    fill(100);
-    circle(this.centre.x, this.centre.y, this.radius * 2);
+    stroke(this.color + 100);
+    fill(this.color);
+    beginShape();
+    for (let i = 0; i < this.numVertices; i++) {
+      vertex(this.vertices[i].x, this.vertices[i].y);
+    }
+    endShape();
   }
 
   update() {
-    this.centre.add(this.velocity); // framerate dependent
-
-    if (this.centre.x + this.radius > width || this.centre.x - this.radius < 0)
-      this.velocity.x = -this.velocity.x;
-    if (this.centre.y + this.radius > height || this.centre.y - this.radius < 0)
-      this.velocity.y = -this.velocity.y;
-
+    let wallXCollided = false,
+      wallYCollided = false;
+    // framerate dependent
+    this.centre.add(this.velocity);
+    for (let i = 0; i < this.numVertices; i++) {
+      let vertex = this.vertices[i];
+      vertex.add(this.velocity);
+      if (vertex.x > width || vertex.x < 0) wallXCollided = true;
+      if (vertex.y > height || vertex.y < 0) wallYCollided = true;
+    }
+    this.calculateEdges();
     this.draw();
+
+    if (wallXCollided) this.velocity.x = -this.velocity.x;
+    if (wallYCollided) this.velocity.y = -this.velocity.y;
   }
 }
--- collisions/sketch.js
+++ collisions/sketch.js
@@ -4,38 +4,45 @@ const bgColor = "#070831"; // (7, 8, 49)
 const primaryColor = "#fff4d6"; // (255, 244, 214)
 const secondaryColor = "#908a76"; // (144, 138, 118)
 
-let body1 = null;
-let body2 = null;
-let bodySqDist = 0;
+let poly1 = null;
+let poly2 = null;
+let polySqDist = 0;
 
 function windowResized() {
   background(bgColor); // to fix color changing on window resize
 }
 
 function setup() {
-  createCanvas(800, 800);
-  body1 = new Body(createVector(200, 200), 80, 2);
-  body2 = new Body(createVector(80, 80), 60, 5);
+  createCanvas(800, 500);
+  fill(100);
+  poly1 = new Polygon(createVector(300, 300), 5, 100, 2, 100, 3);
+  poly2 = new Polygon(createVector(100, 100), 3, 50, 3, 100, 5);
 }
 
 function draw() {
   background(220);
-  collisionCheck(body1, body2);
-  body1.update();
-  body2.update();
+  // collisionCheck(poly1, poly2);
+  poly1.update();
+  poly2.update();
 }
 
-function collisionCheck(body1, body2) {
+function collisionCheck(poly1, poly2) {
   if (
-    createVector(body1.centre.x, body1.centre.y).sub(body2.centre).magSq() <=
-    Math.pow(body1.radius + body2.radius, 2)
+    createVector(poly1.centre.x, poly1.centre.y).sub(poly2.centre).magSq() <=
+    Math.pow(poly1.radius + poly2.radius, 2)
   ) {
-    collisionResolution(body1, body2);
+    collisionResolution(poly1, poly2);
+  } else {
+    poly1.color = 100;
+    poly2.color = 100;
   }
 }
 
-function collisionResolution(body1, body2) {
-  let body1Velocity = body1.velocity.copy(); // Incorrect resolution
-  body1.velocity = body2.velocity.copy();
-  body2.velocity = body1Velocity;
+function collisionResolution(poly1, poly2) {
+  // let poly1Velocity = poly1.velocity.copy(); // Incorrect resolution
+  // poly1.velocity = poly2.velocity.copy();
+  // poly2.velocity = poly1Velocity;
+
+  poly1.color = 200;
+  poly2.color = 200;
 }
